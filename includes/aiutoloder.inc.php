a6ebf7019681aa83af6dcd175e19ef3ee70fafed
nurgit <nurmohammad.aiub@gmail.com>
2022-06-23 17:03:27 +0600

    autoloder eror handaling

--- includes/aiutoloder.inc.php
+++ includes/aiutoloder.inc.php
@@ -6,6 +6,10 @@ function myAutoLoder($className){
     $extension=".class.php";
     $fullPath=$path . $className . $extension;
 
+    if(! file_exists($fullPath)){
+        return false;
+    }
+
     include_once $fullPath;
 
 
